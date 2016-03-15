4be0572a1574621f3122d8ca55c8fe690a96aa86
ypçter <et.bourreau@laposte.net>
2016-03-15 15:25:05 +0100

    Ajout afficherLesSalles capacité et heure d'ouverture

--- controllers/ctrl_salle.py
+++ controllers/ctrl_salle.py
@@ -6,7 +6,7 @@ def afficherLesSalles():
     """
     Fournit à la vue HTML la liste de toutes les salles avec l'ensemble des informations
     """
-    rowsSalles =db(db.salle.categorie_id==db.categorie.id).select(db.salle.id,db.salle.nom,db.categorie.nom) #requête permettant de récupérer les informations des salles
+    rowsSalles =db(db.salle.categorie_id==db.categorie.id).select(db.salle.id,db.salle.nom,db.salle.capacite,db.categorie.nom,db.categorie.heureOuverture,db.categorie.heureOuvertureMinutes) #requête permettant de récupérer les informations des salles
 
     return locals()
 
